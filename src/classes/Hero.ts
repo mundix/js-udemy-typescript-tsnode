d21018e3d22529fa8aabc629e56b4e28b8fde730
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-11-24 04:49:33 -0400

    Testing Export default

---
+++ src/classes/Hero.ts
@@ -0,0 +1,15 @@
+
+export class Hero {
+    constructor(
+        public name: string,
+        public powerId: number,
+        public age: number,
+    ){}
+}
+
+export class Hero2 {}
+export class Hero3 {}
+export class Hero4 {}
+
+export const PI = 3.1416;
+export const miNombre = 'Edmundo';
---
+++ src/data/powers.ts
@@ -0,0 +1,19 @@
+export interface Power {
+    id:number,
+    desc: string
+}
+//  export const powers: Power[] = [
+ const powers: Power[] = [
+     {
+        id:1,
+        desc: 'Money'
+     },
+     {
+        id:2,
+        desc: 'Droga'
+     }
+ ];
+
+
+//  Si queiro exportarla por defecto quito el export de arribay aqui la pongo  
+export default powers;
