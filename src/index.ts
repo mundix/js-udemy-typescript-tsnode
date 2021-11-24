943bf10cf9102cd9223efb367ac68a3ba1b4f7d3
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-11-24 04:50:40 -0400

    Clean Example Hero class

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
+import { Hero  } from './classes/Hero';
 // import { Hero as Superhero, Hero2 } from './classes/Hero';
 // Si quiero imprortarlo todo 
-import  * as HeroClasses from './classes/Hero';
-import powers, {Power} from './data/powers';
+// import  * as HeroClasses from './classes/Hero';
+// import powers, {Power} from './data/powers';
 
-const Hero = 100; //si desea usar una variable hero. 
+// const Hero = 100; //si desea usar una variable hero. 
 
-// const ironman = new Hero('Ironman', 1000, 55);
+const ironman = new Hero('Ironman', 1000, 55);
 // const ironman = new Superhero('Ironman', 1000, 55);
-const ironman = new HeroClasses.Hero('Ironman', 1000, 55); // Haciendo referencia a HeroClasses. 
+// const ironman = new HeroClasses.Hero('Ironman', 1000, 55); // Haciendo referencia a HeroClasses. 
 console.log({ironman});
-console.log({powers});
+// console.log(powers);
 
 
 
