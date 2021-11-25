b4463e96cb7b11714e8f95556a77e55b890b4f85
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-11-25 03:55:43 -0400

    Generando con quicktype.io el interface pokemon

--- src/generics/get-pokemon.ts
+++ src/generics/get-pokemon.ts
@@ -3,11 +3,13 @@ import { Pokemon } from '../interfaces';
 
 // Devuelve tipod e dato Promice por que es un async , de tipo number 
 // export const getPokemon = async( pokemonId: number): Promise<number> => {
-export const getPokemon = async( pokemonId: number): Promise<string> => {
+// export const getPokemon = async( pokemonId: number): Promise<string> => {
+export const getPokemon = async( pokemonId: number): Promise<Pokemon> => {
     const url:string = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
-    const resp = await axios.get(url);
-    console.log(resp.data);
+    // const resp = await axios.get<Pokemon>(url);
+    const {data} = await axios.get<Pokemon>(url);
+    // console.log(resp.data.abilities[0].ability.name);
 
     // return 1;
-    return 'Hola mundo';
+    return data;
 }
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { getPokemon } from './generics/get-pokemon';
 
 
 getPokemon(4)
-    .then( resp => console.log(resp.toUpperCase()))
+    // .then( resp => console.log(resp))
+    .then( pokemon => console.log(pokemon.sprites.front_default))
     .catch(err => console.log(err))
     .finally( () => console.log('Fin de getPokemon'))
 
