8d2426807e6cab05ef79e3251bba54741426b0ed
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-11-25 01:52:14 -0400

    Trabajando conf unciones genericas agregando el <T>

--- generics/generics.ts
+++ generics/generics.ts
@@ -3,6 +3,16 @@ export const printObject = (argument: any) => {
     console.log(argument);
 }
 
-export function genericFunction( argument: any) {
+// export function genericFunction( argument: any) {
+// Para tipo de datos generico se le pone <> y dentro una T , peude tener letras que significa que es generico 
+// Tambien eude tener <TYPE> , puede ser cualqueir palabra que nos ea reservada o cualquier letra 
+// El <T> es recibier un tipod e dato del argumeto
+// El argumento tambien tenra un tipoid e dato T , y el retorono es de tipo generico T 
+export function genericFunction<T>( argument: T): T {
     return argument;
 }
+
+// export const genericFunctionArrow = <T> (argument: T) => {
+//         return argument;
+// }
+export const genericFunctionArrow = <T> (argument: T) => argument;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { printObject, genericFunction } from '../generics/generics';
+import { printObject, genericFunction, genericFunctionArrow } from '../generics/generics';
 
 printObject( 123);
 printObject( new Date());
@@ -6,6 +6,11 @@ printObject( {a:1, b:2 , c:3});
 printObject([1,2,3,4,5,6]);
 printObject('Hola Mundo');
 
+const name: string = 'Edmundo';
+
 console.log(genericFunction(3.1416).toFixed(2));
-console.log(genericFunction('Hola Mundo').toFixed(2));
-console.log(genericFunction(new Date()).toFixed(2));
+console.log(genericFunction(name).toUpperCase());
+// console.log(genericFunction('Hola Mundo').toFixed(2));
+console.log(genericFunction(new Date()).getDate());
+
+console.log(genericFunctionArrow(name).toLowerCase());
