18b1b5d5ebe7dc2380e3434eeeca19be18c2e952
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-11-27 04:06:01 -0400

    Implementando decorador con funcion de flecha y pasando argumento

--- src/decorators/pokemon-class.ts
+++ src/decorators/pokemon-class.ts
@@ -4,9 +4,24 @@
 function printToConsole( constructor: Function) {
     console.log(constructor);
 }
+
+// Para que esto sea un factory consutrcto, debe retornar una funcion 
+const printToConsoleConditional = (print: boolean = false) : Function => {
+    // Voy hacer un return que devuevle una funciona
+    // return () => console.log('hola mundo');
+    if(print) {
+        return printToConsole;
+    }else {
+        // Envio una funcion vacia 
+        return () => {}
+    }
+}
+
 //Ya con esto debe funcionar hay que ponerlo en el tsconfig o jsconfig 
 // Esto solo funciona al momento de compilar el codigo , cuando le pone el decorador @printToConsole
-@printToConsole 
+// @printToConsole 
+// Puedo pmandarle argumento 
+@printToConsoleConditional( false)
 export class Pokemon {
 
     public  publicApi: string = 'https://pokeapi.co';
